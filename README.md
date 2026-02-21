2cac431f55be6dd0c0c481888504e9fe42123802
Cody Baker <cody.c.baker.phd@gmail.com>
2026-02-21 14:25:14 -0500

    setting up

--- README.md
+++ README.md
@@ -3,3 +3,51 @@
 A subset of `content-id-to-dandiset-paths` which have been identified to have only a single correspondence.
 
 We reserve the right to apply special rules for resolving some of these uniqueness mappings, such as manually identifying a common 'parent' for an asset linked to multiple child collections.
+
+Updated frequently.
+
+Primarily for use by developers.
+
+
+
+## One-time use
+
+If you only plan to use this cache infrequently or from disparate locations, you can directly download the latest version of the cache as a minified and compressed JSON file:
+
+### Python API (recommended)
+
+```python
+import gzip
+import json
+import requests
+
+url = "https://raw.githubusercontent.com/dandi-cache/content-id-to-unique-dandiset-path/refs/heads/min/derivatives/content_id_to_unique_dandiset_path.min.json.gz"
+response = requests.get(url)
+content_id_to_unique_dandiset_path = json.loads(gzip.decompress(data=response.content))
+```
+
+### Save to file
+
+```bash
+curl https://raw.githubusercontent.com/dandi-cache/content-id-to-unique-dandiset-path/refs/heads/min/derivatives/content_id_to_unique_dandiset_path.min.json.gz -o content_id_to_unique_dandiset_path.min.json.gz
+```
+
+
+
+## Repeated use
+
+If you plan on using this cache regularly, clone this repository:
+
+```bash
+git clone https://github.com/dandi-cache/content-id-to-unique-dandiset-path.git
+```
+
+Then set up a CRON on your system to pull the latest version of the cache at your desired frequency.
+
+For example, through `crontab -e`, add:
+
+```bash
+0 0 * * * git -C /path/to/content-id-to-unique-dandiset-path pull
+```
+
+This will minimize data overhead by only loading the most recent changes.
--- code/update.py
+++ code/update.py
@@ -1,3 +1,5 @@
+import gzip
+import json
 import pathlib
 
 import requests
@@ -5,21 +7,34 @@ import yaml
 
 
 def _run(base_directory: pathlib.Path, /) -> None:
-    # Download the cache data
-    cache_url = "https://raw.githubusercontent.com/dandi-cache/content-id-to-dandiset-paths/main/cache.json"
-    response = requests.get(cache_url)
-    cache = response.json()
+    url = "https://raw.githubusercontent.com/dandi-cache/content-id-to-dandiset-paths/refs/heads/min/derivatives/content_id_to_dandiset_paths.min.json.gz"
+    response = requests.get(url)
+    content_id_to_dandiset_paths = json.loads(gzip.decompress(data=response.content))
 
-    # Filter to only content IDs with unique file paths
-    unique_cache = {content_id: paths[0] for content_id, paths in cache.items() if len(paths) == 1}
+    content_id_to_unique_dandiset_path: dict[str, dict[str, list[str]]] = dict()
+    multiple_dandisets: dict[str, dict[str, list[str]]] = dict()
+    multiple_paths_same_dandiset: dict[str, dict[str, list[str]]] = dict()
 
-    # Write to YAML in derivatives directory
-    derivatives_dir = base_directory / "derivatives"
-    derivatives_dir.mkdir(exist_ok=True)
+    for content_id, dandisets in content_id_to_dandiset_paths.items():
+        if len(dandisets) > 1:
+            multiple_dandisets[content_id] = dandisets
+            continue
 
-    output_file = derivatives_dir / "unique_content_id_to_path.yaml"
-    with output_file.open("w") as f:
-        yaml.dump(unique_cache, f, default_flow_style=False, sort_keys=True)
+        dandiset_id, paths = next(iter(dandisets.items()))
+        if len(paths) > 1:
+            multiple_paths_same_dandiset[content_id] = {dandiset_id: paths}
+            continue
+
+        content_id_to_unique_dandiset_path[content_id] = {dandiset_id: paths}
+
+    outputs = {
+        "content_id_to_unique_dandiset_path.yaml": content_id_to_unique_dandiset_path,
+        "multiple_dandisets.yaml": multiple_dandisets,
+        "multiple_paths_same_dandiset.yaml": multiple_paths_same_dandiset,
+    }
+    for filename, data in outputs.items():
+        with (base_directory / "derivatives" / filename).open(mode="w") as file_stream:
+            yaml.safe_dump(data=data, stream=file_stream)
 
 
 if __name__ == "__main__":
